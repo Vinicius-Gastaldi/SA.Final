8b563d910ce0fdcad6f597a301b71089b07c79be
Vinícius_Gastaldi <150020388+Vinicius-Gastaldi@users.noreply.github.com>
2024-11-07 08:16:37 -0300

    Add files via upload

--- SA.Final/ShutdawnDB.sql
+++ SA.Final/ShutdawnDB.sql
@@ -16,19 +16,26 @@
 
 
 -- Copiando estrutura do banco de dados para shutdawndb
+DROP DATABASE IF EXISTS `shutdawndb`;
 CREATE DATABASE IF NOT EXISTS `shutdawndb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `shutdawndb`;
 
 -- Copiando estrutura para tabela shutdawndb.acesso
+DROP TABLE IF EXISTS `acesso`;
 CREATE TABLE IF NOT EXISTS `acesso` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.acesso: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.acesso: ~3 rows (aproximadamente)
+REPLACE INTO `acesso` (`id`, `nome`) VALUES
+	(1, 'Ativo'),
+	(2, 'Inativo'),
+	(3, 'Bloqueado');
 
 -- Copiando estrutura para tabela shutdawndb.desenvolvedor
+DROP TABLE IF EXISTS `desenvolvedor`;
 CREATE TABLE IF NOT EXISTS `desenvolvedor` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
@@ -37,51 +44,95 @@ CREATE TABLE IF NOT EXISTS `desenvolvedor` (
   PRIMARY KEY (`id`),
   KEY `squadId` (`squadId`),
   CONSTRAINT `desenvolvedor_ibfk_1` FOREIGN KEY (`squadId`) REFERENCES `squad` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.desenvolvedor: ~5 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.desenvolvedor: ~6 rows (aproximadamente)
 REPLACE INTO `desenvolvedor` (`id`, `nome`, `phone`, `squadId`) VALUES
 	(2, 'Jão', '999221478', 1),
 	(3, 'Armenio', '999221478', 1),
 	(4, 'Armenio', '999221478', 1),
-	(5, 'Armenio', '999221478', 1),
+	(5, 'lucas', '9912224124', 2),
 	(6, 'Armenio', '999221478', 1),
 	(7, 'ovos', '999221478', 1);
 
+-- Copiando estrutura para tabela shutdawndb.page
+DROP TABLE IF EXISTS `page`;
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `page_url` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `page_url` (`page_url`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela shutdawndb.page: ~1 rows (aproximadamente)
+REPLACE INTO `page` (`id`, `page_url`, `description`) VALUES
+	(1, '/WEB1_war/home.html', NULL);
+
 -- Copiando estrutura para tabela shutdawndb.papel
+DROP TABLE IF EXISTS `papel`;
 CREATE TABLE IF NOT EXISTS `papel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.papel: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.papel: ~1 rows (aproximadamente)
 REPLACE INTO `papel` (`id`, `nome`) VALUES
 	(1, 'admin');
 
+-- Copiando estrutura para tabela shutdawndb.papel_page_permissao
+DROP TABLE IF EXISTS `papel_page_permissao`;
+CREATE TABLE IF NOT EXISTS `papel_page_permissao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `papel_id` int(11) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `papel_id` (`papel_id`,`page_id`),
+  KEY `page_id` (`page_id`),
+  CONSTRAINT `papel_page_permissao_ibfk_1` FOREIGN KEY (`papel_id`) REFERENCES `papel` (`id`),
+  CONSTRAINT `papel_page_permissao_ibfk_2` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela shutdawndb.papel_page_permissao: ~1 rows (aproximadamente)
+REPLACE INTO `papel_page_permissao` (`id`, `papel_id`, `page_id`) VALUES
+	(3, 1, 1);
+
 -- Copiando estrutura para tabela shutdawndb.squad
+DROP TABLE IF EXISTS `squad`;
 CREATE TABLE IF NOT EXISTS `squad` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `descricao` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.squad: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.squad: ~4 rows (aproximadamente)
 REPLACE INTO `squad` (`id`, `nome`, `descricao`) VALUES
 	(1, 'jonasponas', NULL),
-	(2, 'Armenio', NULL);
+	(2, 'Armenio', NULL),
+	(4, 'RH', 'PessoaldeHumanas'),
+	(5, 'RH', 'PessoaldeHumanas');
 
 -- Copiando estrutura para tabela shutdawndb.status
+DROP TABLE IF EXISTS `status`;
 CREATE TABLE IF NOT EXISTS `status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.status: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.status: ~6 rows (aproximadamente)
+REPLACE INTO `status` (`id`, `nome`) VALUES
+	(1, 'Backlog'),
+	(2, 'a fazer'),
+	(3, 'fazendo'),
+	(4, 'testando'),
+	(5, 'pronto'),
+	(6, 'entregue');
 
 -- Copiando estrutura para tabela shutdawndb.tarefa
+DROP TABLE IF EXISTS `tarefa`;
 CREATE TABLE IF NOT EXISTS `tarefa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `desenvolvedor_id` int(11) DEFAULT NULL,
@@ -104,6 +155,7 @@ CREATE TABLE IF NOT EXISTS `tarefa` (
 -- Copiando dados para a tabela shutdawndb.tarefa: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela shutdawndb.tipo_tarefa
+DROP TABLE IF EXISTS `tipo_tarefa`;
 CREATE TABLE IF NOT EXISTS `tipo_tarefa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(255) NOT NULL,
@@ -114,6 +166,7 @@ CREATE TABLE IF NOT EXISTS `tipo_tarefa` (
 -- Copiando dados para a tabela shutdawndb.tipo_tarefa: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela shutdawndb.usuario
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
@@ -126,10 +179,9 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   CONSTRAINT `fk_acesso_id` FOREIGN KEY (`acesso_id`) REFERENCES `acesso` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela shutdawndb.usuario: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela shutdawndb.usuario: ~2 rows (aproximadamente)
 REPLACE INTO `usuario` (`id`, `email`, `senha`, `dev_id`, `papel_id`, `acesso_id`) VALUES
 	(1, 'admin@gmail.com', '1234', NULL, 1, NULL),
-	(4, 'outroUsu@gmail.com', '6789', 2, 1, NULL),
 	(5, 'NicolasSezerino@gmail', '420', 3, 1, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
